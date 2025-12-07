1288b016f261c2721ab2036d3570c7278a0684a8
Martin Heidegger <martin.heidegger@gmail.com>
2025-12-07 23:52:23 +0900

    format template

--- template/index.mjs
+++ template/index.mjs
@@ -3,21 +3,19 @@ import { readFileSync } from 'node:fs'
 import { deepEqual } from 'node:assert'
 
 const inputFolder = (() => {
-    const paths = (new URL('.', import.meta.url)).pathname.split('/')
-    paths.pop()
-    const date = paths.slice(paths.length - 2).join('/')
-    return new URL(`../../input/${date}/`, import.meta.url)
+  const paths = new URL('.', import.meta.url).pathname.split('/')
+  paths.pop()
+  const date = paths.slice(paths.length - 2).join('/')
+  return new URL(`../../input/${date}/`, import.meta.url)
 })()
 
 const input = readFileSync(new URL('input', inputFolder), 'utf8')
 const test = readFileSync(new URL('test', inputFolder), 'utf8')
 
-function partOne() {
-}
+function partOne() {}
 
 console.log(partOne())
 
-function partTwo() {
-}
+function partTwo() {}
 
 console.log(partTwo())
